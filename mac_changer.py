6eed43fe80e11673a228ce7b7763bdf42e88ad8a
Jay Sim <jay.sim@vshn.net>
2023-02-27 21:36:40 -0800

    Algorithms added

--- mac_changer.py
+++ mac_changer.py
@@ -2,19 +2,38 @@
 
 import subprocess
 import optparse
+import re
 
-parser = optparse.OptionParser()
 
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info")
+    elif not options.new_mac:
+        parser.error("[-] Please specify an new mac, use --help for more info")
+    return options
 
-(options, arguments) = parser.parse_args()
 
-interface = options.interface
-new_mac = options.new_mac
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+options = get_arguments()
+# change_mac(options.interface, options.new_mac)
+
+
+ifconfig_result_decode = subprocess.check_output(["ifconfig", options.interface])
+ifconfig_result = ifconfig_result_decode.decode('utf-8')
+
+mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+if mac_address_search_result:
+    print(mac_address_search_result.group(0))
+else:
+    print("[-] Could not read MAC address")
